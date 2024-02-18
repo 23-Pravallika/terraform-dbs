645e75d98f60c7f6d88f38c6037505042c70f118
23-Pravallika <learngain23@gmail.com>
2024-02-18 15:42:05 +0530

    AutoPush

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ module "docdb" {
 module "redis" {
     source = "./vendor/modules/redis" 
     ENV = var.ENV
+    REDIS_NODE_TYPE = var.REDIS_NODE_TYPE
+    REDIS_ENGINE_VERSION = var.REDIS_ENGINE_VERSION
+    REDIS_PORT = var.REDIS_PORT
+    REDIS_NODE_COUNT = var.REDIS_NODE_COUNT
 
 }
 
