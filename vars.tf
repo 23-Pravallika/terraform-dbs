2e1f4320dea91b3ef826d75892d93bf146dbffc4
23-Pravallika <learngain23@gmail.com>
2024-02-22 11:26:21 +0530

    AutoPush

--- vars.tf
+++ vars.tf
@@ -7,3 +7,16 @@ variable "REDIS_PORT" {}
 variable "REDIS_NODE_COUNT" {}
 
 
+# Docdb or Mongodb
+variable "DOCDB_PORT" {}
+variable "DOCDB_INSTANCE_CLASS" {}
+variable "DOCDB_INSTANCE_COUNT" {}
+
+
+# RDS or MySql
+variable "RDS_PORT" {}
+variable "RDS_STORAGE" {}
+variable "RDS_ENGINE_VERSION" {}
+variable "RDS_INSTANCE_CLASS" {}
+
+
