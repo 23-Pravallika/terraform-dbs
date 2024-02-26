1a19a404fd13992a78ea020cd76560aa9a556cfa
23-Pravallika <learngain23@gmail.com>
2024-02-26 19:27:16 +0530

    AutoPush

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "DOCDB_ENDPOINT"{
+    value = module.docdb.DOCDB_ENDPOINT
+}
+
+output "REDIS_ENDPOINT"{
+    value = module.redis.REDIS_ENDPOINT
+}
+
+output "MYSQL_ENDPOINT"{
+    value = module.mysql.MYSQL_ENDPOINT
+}
+
+output "RABBITMQ_ENDPOINT"{
+    value = module.rabbitmq.RABBITMQ_ENDPOINT
+}
+
+
