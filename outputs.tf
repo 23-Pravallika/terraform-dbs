ff17428414675569a4970b2e3ce047739fb9e5bc
23-Pravallika <learngain23@gmail.com>
2024-02-26 19:37:15 +0530

    AutoPush

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "DOCDB_ENDPOINT"{
     value = module.docdb.DOCDB_ENDPOINT
 }
 
-output "REDIS_ENDPOINT"{
-    value = module.redis.REDIS_ENDPOINT
-}
-
 output "MYSQL_ENDPOINT"{
     value = module.mysql.MYSQL_ENDPOINT
 }
